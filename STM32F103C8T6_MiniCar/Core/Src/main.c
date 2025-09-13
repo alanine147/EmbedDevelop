fe9bf257e7ecffa06eb916a9067ae954e40c4a2e
alanine1 <gcg2357@163.com>
2025-09-14 00:21:33 +0800

    使用ticks作为时间戳,关闭RTC

--- STM32F103C8T6_MiniCar/Core/Src/main.c
+++ STM32F103C8T6_MiniCar/Core/Src/main.c
@@ -45,8 +45,6 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c2;
 
-RTC_HandleTypeDef hrtc;
-
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
@@ -73,7 +71,6 @@ static void MX_TIM4_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C2_Init(void);
-static void MX_RTC_Init(void);
 void TaskForegroundEntry(void const * argument);
 extern void TaskBackgroundEntry(void const * argument);
 
@@ -119,7 +116,6 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_I2C2_Init();
-  MX_RTC_Init();
   /* USER CODE BEGIN 2 */
   CDD_RB_Init(&cdd_rb_cfg);
   sys_log("MiniCar startup, compiler at %s, %s", __DATE__, __TIME__);
@@ -177,15 +173,13 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
@@ -205,12 +199,6 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
@@ -247,64 +235,6 @@ static void MX_I2C2_Init(void)
   /* USER CODE BEGIN I2C2_Init 2 */
 
   /* USER CODE END I2C2_Init 2 */
-
-}
-
-/**
-  * @brief RTC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_RTC_Init(void)
-{
-
-  /* USER CODE BEGIN RTC_Init 0 */
-
-  /* USER CODE END RTC_Init 0 */
-
-  RTC_TimeTypeDef sTime = {0};
-  RTC_DateTypeDef DateToUpdate = {0};
-
-  /* USER CODE BEGIN RTC_Init 1 */
-
-  /* USER CODE END RTC_Init 1 */
-
-  /** Initialize RTC Only
-  */
-  hrtc.Instance = RTC;
-  hrtc.Init.AsynchPrediv = RTC_AUTO_1_SECOND;
-  hrtc.Init.OutPut = RTC_OUTPUTSOURCE_NONE;
-  if (HAL_RTC_Init(&hrtc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /* USER CODE BEGIN Check_RTC_BKUP */
-
-  /* USER CODE END Check_RTC_BKUP */
-
-  /** Initialize RTC and set the Time and Date
-  */
-  sTime.Hours = 0x0;
-  sTime.Minutes = 0x0;
-  sTime.Seconds = 0x0;
-
-  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  DateToUpdate.WeekDay = RTC_WEEKDAY_MONDAY;
-  DateToUpdate.Month = RTC_MONTH_JANUARY;
-  DateToUpdate.Date = 0x1;
-  DateToUpdate.Year = 0x0;
-
-  if (HAL_RTC_SetDate(&hrtc, &DateToUpdate, RTC_FORMAT_BCD) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN RTC_Init 2 */
-
-  /* USER CODE END RTC_Init 2 */
 
 }
 
--- STM32F103C8T6_MiniCar/User/src/real_clock.c
+++ STM32F103C8T6_MiniCar/User/src/real_clock.c
@@ -7,7 +7,6 @@
 
 #include "real_clock.h"
 #include "stm32f1xx_hal.h"
-#include "stm32f1xx_ll_rtc.h"
 #include "common.h"
 
 /************************************************************************
@@ -41,42 +40,18 @@
 /************************************************************************
  * Local function declarations
  ************************************************************************/
-static inline uint32_t RTC_ReadTimeCounter(RTC_TypeDef *RTCx);
+
 /************************************************************************
  * Function implementation
  ************************************************************************/
 
 RealClockTimeType TimeNowGet(void)
 {
-    RealClockTimeType timeCur;
-    timeCur.microSec = (LSE_VALUE - LL_RTC_GetDivider(hrtc.Instance)) * 1000 / LSE_VALUE;
-    timeCur.sec = RTC_ReadTimeCounter(hrtc.Instance);
+    RealClockTimeType timeCur = {0u};
+    uint32 ticks = HAL_GetTick();
+    timeCur.microSec = ticks % 1000u;
+    timeCur.sec = ticks / 1000u;
     return timeCur;
-}
-
-static inline uint32_t RTC_ReadTimeCounter(RTC_TypeDef *RTCx)
-{
-  uint16_t high1 = 0U, high2 = 0U, low = 0U;
-  uint32_t timecounter = 0U;
-
-  high1 = READ_REG(RTCx->CNTH & RTC_CNTH_RTC_CNT);
-  low   = READ_REG(RTCx->CNTL & RTC_CNTL_RTC_CNT);
-  high2 = READ_REG(RTCx->CNTH & RTC_CNTH_RTC_CNT);
-
-  if (high1 != high2)
-  {
-    /* In this case the counter roll over during reading of CNTL and CNTH registers,
-       read again CNTL register then return the counter value */
-    timecounter = (((uint32_t) high2 << 16U) | READ_REG(RTCx->CNTL & RTC_CNTL_RTC_CNT));
-  }
-  else
-  {
-    /* No counter roll over during reading of CNTL and CNTH registers, counter
-       value is equal to first value of CNTL and CNTH */
-    timecounter = (((uint32_t) high1 << 16U) | low);
-  }
-
-  return timecounter;
 }
 
 
